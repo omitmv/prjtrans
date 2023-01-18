aa09d92cebded5640388aadc41486d1b1ff2ec53
Rafael Valverde <omit.mv@gmail.com>
2023-01-18 13:16:51 -0300

    Correções...

--- routes/ticket/index.js
+++ routes/ticket/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ silent: true })
 const express = require('express')
 const router = express.Router()
 const oracledb = require('oracledb')
--- utils/cripto.js
+++ utils/cripto.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ silent: true })
 const jwt = require('jsonwebtoken')
 
 const criptGetToken = value => {
